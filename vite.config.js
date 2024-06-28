0ebe5fd5f1dc8eb3a34c0ea93017066938003d39
Ihorsoft <0639492002s@gmail.com>
2024-06-28 13:30:46 +0300

    add import react from  -swc

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
- */
+ content from package Vite for react */
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+//import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
