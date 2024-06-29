6fe6939f455f476642c1add40278c56fe3561115
Ihorsoft <0639492002s@gmail.com>
2024-06-29 12:17:36 +0300

    2024-06-29

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import React from "react";
+// import ReactDOM from "react-dom/client";
+
+//import { useState } from "react";
 //import reactLogo from "./assets/react.svg";
 //import viteLogo from "/vite.svg";
 import "./App.css";
@@ -10,36 +13,6 @@ import { Profile } from "./Profile";
 import userData from "../userData1.json";
 
 // const userData = JSON.parse(userData1);
-
-/* function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + my first R project</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
- */
 
 //const message = ["goodbay", "thefinish"];
 
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -3,14 +3,11 @@ const MessageList = ({ messages }) => {
   return (
     <div>
       {messages.map((item, index) => {
-        // console.log("element array", item);
-
         return (
           <p key={item}>
             Message {index + 1} {item}
           </p>
         );
-        // <p key={item}>{item}</p>;
       })}
     </div>
   );
